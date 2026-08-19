c96ef762cf1edeaf91f68088ffd07f1252c45926
littlechai <268562069+littlechai@users.noreply.github.com>
2026-08-19 04:02:41 +0000

    chore(deps): bump sherpa-onnx to v1.13.6

--- scripts/sherpa-onnx-vars.sh
+++ scripts/sherpa-onnx-vars.sh
@@ -28,11 +28,11 @@ sherpa_onnx_set_vars() {
     SHERPA_ONNX_SHA256=""
 
     case "${version}" in
-        1.13.2)
+        1.13.6)
             if [[ "${target_arch}" == "aarch64" ]]; then
-                SHERPA_ONNX_SHA256="b54178420e9e6ff6c7f308b5f1cde827215b38393356ee0bd2b7595c648b330b"
+                SHERPA_ONNX_SHA256="3a11e89b5a12cd1f8809e385087df7194e8463ab240cf530e5d7cc6d7b27fae7"
             elif [[ "${target_arch}" == "x86_64" ]]; then
-                SHERPA_ONNX_SHA256="a1fb5d71811e85fe1fd5856540c6d19614681e222810e266d3b33038f724be75"
+                SHERPA_ONNX_SHA256="089c01bded9166ed53f9e433b030863f9dd5ec2ad067dca73a8ba96dca25979d"
             fi
             ;;
     esac
@@ -52,4 +52,4 @@ sherpa_onnx_fetch_digest() {
              | ltrimstr("sha256:")'
 }
 
-sherpa_onnx_set_vars "${SHERPA_ONNX_VERSION:-1.13.2}"
+sherpa_onnx_set_vars "${SHERPA_ONNX_VERSION:-1.13.6}"
